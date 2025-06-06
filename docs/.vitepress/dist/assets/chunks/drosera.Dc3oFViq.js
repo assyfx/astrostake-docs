35743f07b6860ea8fa0d3f165971ab13218cca13
assyfx <assyfx@github.com>
2025-06-06 21:15:51 +0700

    Add Vercel rewrite config

---
+++ docs/.vitepress/dist/assets/chunks/drosera.Dc3oFViq.js
@@ -0,0 +1,1 @@
+const o="/logos/0g.png",s="/logos/drosera.png";export{o as _,s as a};
--- docs/.vitepress/dist/assets/chunks/vana.ClMTTa1r.js
+++
@@ -1,1 +0,0 @@
-const o="/logos/0g.png",s="/logos/drosera.png",g="/logos/dill.png",n="/logos/vana.png";export{o as _,s as a,g as b,n as c};
